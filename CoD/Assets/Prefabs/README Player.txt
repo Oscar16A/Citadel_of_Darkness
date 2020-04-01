95bf408f7d6c70da3def12d8266880f6f7c2e83f
Oscar Amarillas <oscar16amarillas@gmail.com>
2020-03-31 19:19:58 -0700

    add proto player prefab

---
+++ CoD/Assets/Prefabs/README Player.txt
@@ -0,0 +1,4 @@
+WASD to move
+mouse to look
+change camera sensitivity in the camera axis child object
+for jumping and gravity to work, make sure the level is on the Ground layer
---
+++ CoD/Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement: MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    private Transform playerBody;
+    private Transform playerCamera;
+    private float xRotation = 0f;
+    void Start()
+    {
+        playerBody = transform.parent;
+        playerCamera = transform.Find("Camera");
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
