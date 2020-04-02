e901f1c18bb79ed2696b3ee941cf43aeabee6f0b
Oscar Amarillas <oscar16amarillas@gmail.com>
2020-04-02 13:34:50 -0700

    add interactable objects prototype

---
+++ Assets/Scripts/Player Controls/InteractControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractControl : MonoBehaviour
+{
+    public Transform myCamera;
+    public LayerMask myMask;
+    public float interactDistance = 2f;
+    public GameObject interactText;
+    private void Update()
+    {
+        RaycastHit hit;
+        Physics.Raycast(myCamera.position, myCamera.forward, out hit, interactDistance, myMask);
+        Transform hitTransform = hit.transform;
+        interactText.SetActive(false);
+        if(hitTransform != null)
+        {
+            Interactable hitInteract = hitTransform.GetComponent<Interactable>();
+            if(hitInteract != null)
+            {
+                interactText.SetActive(true);
+                if(Input.GetKeyDown("e"))
+                {
+                    hitInteract.HitTrigger();
+                }
+            }
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(myCamera.position, myCamera.forward * interactDistance);
+    }
+}
---
+++ Assets/Scripts/Test_Button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Button : Interactable
+{
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public override void HitTrigger()
+    {
+        anim.SetTrigger("Button Press");
+    }
+}
