2e4d7dfc4bec9c1b24cba9905ef0b6d2965959de
Aleroms <santiagomorales333@yahoo.com>
2020-04-09 22:58:07 -0700

    Volume & MouseSensitivity Sliders Functional

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 public class MenuUI : MonoBehaviour
 {
+	//MouseMovement mMove;
+	public TMP_Text mouseInput;
+	public TMP_Text volumeInput;
+
+	public void Awake()
+	{
+		//mMove = GetComponent<MouseMovement>();
+	}
     public void Play()
 	{
 		print("Playing next level...");
@@ -15,5 +25,20 @@ public class MenuUI : MonoBehaviour
 		print("Quitting...");
 
 		Application.Quit();
+	}
+	public void MouseSensitivity(float input)
+	{    
+		mouseInput.text = input.ToString("F1");
+		StatsData.mouseSensitivity = input * 10 + 70;
+		print("statsData.mouse=" + StatsData.mouseSensitivity);
+
+		//print(input);
+		// N*10 + 70
+	}
+	public void VolumeLevel(float input)
+	{
+		volumeInput.text = input.ToString();
+		StatsData.volumeLevel = input;
+		print("statsData.volume=" + StatsData.volumeLevel);
 	}
 }
--- Assets/Scripts/StatsData.cs
+++ Assets/Scripts/StatsData.cs
@@ -6,5 +6,7 @@ public static class StatsData
 {
 	public static int collectables = 0;
 
-	
+	public static float mouseSensitivity = 100f;
+
+	public static float volumeLevel = 10f;
 }
