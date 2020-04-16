26895f52fd2729834f7e0cc9d9f1c78c1177a67c
Oscar Amarillas <oscar16amarillas@gmail.com>
2020-04-15 23:19:57 -0700

    add prototype walljump and seperate movement and ledge climb scripts for experimentation

---
+++ Assets/Scripts/Player Controls/ClimbControl2.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbControl2 : MonoBehaviour
+{
+    private PlayerMovement2 movement;
+    private CharacterController controller;
+    private bool validClimb;
+    private LayerMask myMask;
+    public Animator anim;
+    public float angleTolerance = 0f; // how far the ledge can be tillted and still be climbed
+    private void Start()
+    {
+        movement = GetComponent<PlayerMovement2>();
+        controller = GetComponent<CharacterController>();
+        myMask = movement.groundMask;
+    }
+    private void Update()
+    {
+        if(!movement.isGrounded)
+        {
+            RaycastHit hit = new RaycastHit();
+            validClimb = CheckClimb(ref hit);
+            if(Input.GetKey("w") && validClimb)
+            {
+                ClimbLedge(ref hit);
+            }
+        }
+    }
+
+    private bool CheckClimb(ref RaycastHit hit)
+    {
+        if(Physics.Raycast(transform.position + transform.forward + new Vector3(0f,2f,0f), -transform.up, out hit, 2.5f, myMask))
+        {
+            if(Vector3.Angle(hit.normal, Vector3.up) <= angleTolerance)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void ClimbLedge(ref RaycastHit hit)
+    {
+        movement.freeze = true;
+        controller.Move(transform.forward + new Vector3(0f,hit.point.y-transform.position.y + 1f,0f));
+        anim.SetTrigger("Climb");
+        UnfreezeDelay(0.34f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position + transform.forward + new Vector3(0f,2f,0f), -transform.up*2.5f);
+    }
+
+    private void UnfreezeDelay(float time)
+    {
+        IEnumerator delay = WaitSeconds(time);
+        StartCoroutine(delay);
+    }
+
+    IEnumerator WaitSeconds(float time)
+    {
+        yield return new WaitForSeconds(time);
+        movement.freeze = false;
+    }
+}
---
+++ Assets/Scripts/Player Controls/WallRun.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRun : MonoBehaviour
+{
+    private PlayerMovement2 movement;
+    private CharacterController controller;
+    private LayerMask myMask;
+    private bool validWall;
+    private float startGravity;
+
+    private void Start()
+    {
+        movement = GetComponent<PlayerMovement2>();
+        controller = GetComponent<CharacterController>();
+        myMask = movement.groundMask;
+        startGravity = movement.gravity;
+    }
+
+    private void Update()
+    {
+        RaycastHit hit = new RaycastHit();
+        if(!movement.isGrounded)
+        {
+            if(CheckWall(ref hit) && Input.GetKey("w"))
+            {
+                //Debug.Log(hit.normal + " hit normal");
+                //Debug.Log(Vector3.SignedAngle(hit.normal, transform.forward, Vector3.up) + " compare angle forward");//negative left, positive right
+                Run(ref hit);
+            }
+            else
+            {
+                movement.freezeY = false;
+            }
+        }
+        else
+        {
+            movement.freezeY = false;
+        }
+    }
+
+    private bool CheckWall(ref RaycastHit hit)
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 2f, myMask)) //facing a wall
+        {
+            if(Falling())
+            {
+                //Debug.Log("valid wall");
+                return true;
+            }
+        }
+        //Debug.Log("not valid wall");
+        return false;
+    }
+    private void Run(ref RaycastHit hit)
+    {
+        movement.freezeY = true;
+        if(Input.GetKeyDown("space"))
+        {
+            HopOff(ref hit);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, transform.forward * 2f);
+    }
+
+    private bool Falling()
+    {
+        return controller.velocity.y <= 0f;
+    }
+    private void HopOff(ref RaycastHit hit)
+    {
+        movement.velocity = Vector3.zero;
+        
+
+        if(Vector3.SignedAngle(hit.normal, transform.forward, Vector3.up) > 0f)//wall right, move left
+        {
+            movement.velocity += -transform.right * movement.sprintSpeed;
+
+        }
+        else//wall left, move rigt
+        {
+            movement.velocity += transform.right * movement.sprintSpeed;
+        }
+
+        movement.velocity.y = Mathf.Sqrt(movement.jumpHeight * -2 * startGravity);//add vertical movement
+        movement.freezeY = false;
+    }
+}
