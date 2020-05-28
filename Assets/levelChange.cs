fdccb1841f39defd37981b3c8833722302a8d87d
Gourd (cole) <cmedley@uci.edu>
2020-05-28 11:51:28 -0700

    proto1 and level switch

---
+++ Assets/levelChange.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelChange : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+            SceneManager.LoadScene("Dreary Entrance");
+    }
+}
