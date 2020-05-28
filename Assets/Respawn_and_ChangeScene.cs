5aaef26e8390b27d777c6f9e3f9e6d6f816a788d
Gourd (cole) <cmedley@uci.edu>
2020-05-28 10:59:45 -0700

    pusahing for oscar

---
+++ Assets/Respawn_and_ChangeScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn_and_ChangeScene : MonoBehaviour
+{
+    public Vector3 checkpointPos;
+    public GameObject player;
+
+    private void Start()
+    {
+        checkpointPos = this.transform.position;
+        Debug.Log(checkpointPos);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OOF");
+        if(other.tag == "checkpoint")
+        {
+            Debug.Log("checkpoint!");
+            checkpointPos = other.transform.position;
+            Debug.Log(checkpointPos);
+        }
+
+        else if (other.tag == "killVolume")
+        {
+            Debug.Log("doubleOOF");
+            Debug.Log("stored position:");
+            Debug.Log(checkpointPos);
+            this.transform.position = checkpointPos;
+            Debug.Log("Moved Position:");
+            Debug.Log(player.transform.position);
+        }
+    }
+}
